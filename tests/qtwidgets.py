5b83e05119dc85c47e0905300be33ac20b9f68e5
Stefan Lehmann <stlm@posteo.de>
2018-12-08 21:33:50 +0100

    Add more signal tests.

---
+++ tests/qtwidgets.py
@@ -0,0 +1,19 @@
+"""
+:author: Stefan Lehmann <stlm@posteo.de>
+:license: MIT, see license file or https://opensource.org/licenses/MIT
+
+:created on 2018-12-08 18:13:28
+:last modified by:   Stefan Lehmann
+:last modified time: 2018-12-08 18:18:29
+
+"""
+from PyQt5 import QtWidgets
+
+
+# Check signals
+checkbox = QtWidgets.QCheckBox()
+checkbox.stateChanged.connect(lambda: None)
+checkbox.pressed.connect(lambda: None)
+checkbox.released.connect(lambda: None )
+checkbox.toggled.connect(lambda: None )
+checkbox.clicked.connect(lambda: None )
