ce1a7fbae8623d32021c1d670a52569759824ec0
Stefan Lehmann <stlm@posteo.de>
2019-02-09 18:16:16 +0100

    setup.py: Add description and classifiers

--- setup.py
+++ setup.py
@@ -1,13 +1,40 @@
-"""
+"""Python setup script.
+
 :author: Stefan Lehmann <stlm@posteo.de>
 :license: MIT, see license file or https://opensource.org/licenses/MIT
 
 :created on 2018-10-06 10:55:36
 :last modified by:   Stefan Lehmann
-:last modified time: 2018-12-08 21:43:54
+:last modified time: 2019-02-09 18:13:41
 
 """
-from distutils.core import setup
+import io
+import os
+import re
+from setuptools import setup
+
+
+def read(*names, **kwargs):
+    try:
+        with io.open(
+            os.path.join(os.path.dirname(__file__), *names),
+            encoding=kwargs.get("encoding", "utf8")
+        ) as fp:
+            return fp.read()
+    except IOError:
+        return ''
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+long_description = read('README.md')
 
 
 setup(
@@ -15,8 +42,18 @@ setup(
     url="https://github.com/stlehmann/PyQt5-stubs",
     author="Stefan Lehmann",
     author_email="stlm@posteo.de",
-    version="5.11.3.2",
+    description="PEP561 stub files for the PyQt5 framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=find_version('PyQt5-stubs', '__init__.pyi'),
     package_data={"PyQt5-stubs": ['*.pyi']},
     install_requires=["PyQt5==5.11.3"],
-    packages=["PyQt5-stubs"]
+    packages=["PyQt5-stubs"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development"
+    ]
 )
