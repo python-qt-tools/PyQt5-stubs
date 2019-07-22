de0088f2bc4887c4e613f5c0a376ba1e423f5cd4
Stefan Lehmann <stlm@posteo.de>
2019-07-22 15:23:38 +0200

    merge build scripts to one.

--- buildenv/build_pyqt5.sh
+++ buildenv/build_pyqt5.sh
@@ -1,12 +1,28 @@
 # @Author: Stefan Lehmann
 # @Date:   2018-12-06 11:24:07
 # @Last Modified by:   Stefan Lehmann
-# @Last Modified time: 2019-07-22 13:41:40
-VERSION="5.13.0"
+# @Last Modified time: 2019-07-22 15:22:07
+PYQT_VERSION="5.13.0"
+SIP_VERSION="4.19.18"
 
-wget https://www.riverbankcomputing.com/static/Downloads/PyQt5/$VERSION/PyQt5_gpl-$VERSION.tar.gz -O PyQt5.tar.gz
+# build sip
+cd /root
+wget https://www.riverbankcomputing.com/static/Downloads/sip/$SIP_VERSION/sip-$SIP_VERSION.tar.gz -O sip.tar.gz
+tar -xvzf sip.tar.gz
+cd sip-*
+python3 configure.py --sip-module PyQt5.sip
+make
+make install
+cd /root
+
+# build PyQt5
+wget https://www.riverbankcomputing.com/static/Downloads/PyQt5/$PYQT_VERSION/PyQt5_gpl-$PYQT_VERSION.tar.gz -O PyQt5.tar.gz
 tar -xvzf PyQt5.tar.gz
-cd /root/PyQt5_gpl-$VERSION
+cd PyQt5_gpl-$PYQT_VERSION
 python3 configure.py --confirm-license
 make
 make install
+
+# copy stubs
+cd /root
+cp /root/PyQt5_gpl-$PYQT5_VERSION/*.pyi /root/stubs/
