05ac7a25acd8c3ce8c0c7bfef44c46c44fa306c5
Stefan Lehmann <stlm@posteo.de>
2019-07-22 13:52:03 +0200

    Use PyQt5.13.0 and sip4.19.18

--- buildenv/build_pyqt5.sh
+++ buildenv/build_pyqt5.sh
@@ -1,10 +1,12 @@
 # @Author: Stefan Lehmann
 # @Date:   2018-12-06 11:24:07
 # @Last Modified by:   Stefan Lehmann
-# @Last Modified time: 2018-12-06 11:25:36
-wget https://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.11.3/PyQt5_gpl-5.11.3.tar.gz/download -O PyQt5.tar.gz
+# @Last Modified time: 2019-07-22 13:41:40
+VERSION="5.13.0"
+
+wget https://www.riverbankcomputing.com/static/Downloads/PyQt5/$VERSION/PyQt5_gpl-$VERSION.tar.gz -O PyQt5.tar.gz
 tar -xvzf PyQt5.tar.gz
-cd /root/PyQt5_gpl-5.11.3
+cd /root/PyQt5_gpl-$VERSION
 python3 configure.py --confirm-license
 make
 make install
--- buildenv/build_sip.sh
+++ buildenv/build_sip.sh
@@ -1,10 +1,12 @@
 # @Author: Stefan Lehmann
 # @Date:   2018-12-06 11:22:17
 # @Last Modified by:   Stefan Lehmann
-# @Last Modified time: 2018-12-06 16:12:24
-wget https://sourceforge.net/projects/pyqt/files/sip/sip-4.19.13/sip-4.19.13.tar.gz -O sip.tar.gz
+# @Last Modified time: 2019-07-22 13:48:35
+VERSION="4.19.18"
+
+wget https://www.riverbankcomputing.com/static/Downloads/sip/$VERSION/sip-$VERSION.tar.gz -O sip.tar.gz
 tar -xvzf sip.tar.gz
 cd sip-*
-python3 configure.py --sip-module PyQt5.sip --no-tools
+python3 configure.py --sip-module PyQt5.sip
 make
 make install
