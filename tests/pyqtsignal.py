ffd23924e69b25f99f9cf17487688c2e664658aa
Kyle Altendorf <sda@fstab.net>
2020-09-30 23:20:47 -0400

    Add basic 'test'

--- tests/pyqtsignal.py
+++ tests/pyqtsignal.py
@@ -3,8 +3,13 @@ from PyQt5 import QtCore
 
 
 class Class(QtCore.QObject):
-    signal: typing.ClassVar[QtCore.pyqtSignal] = QtCore.pyqtSignal()
+    signal: typing.ClassVar[QtCore.pyqtSignal] = QtCore.pyqtSignal([str])
 
+
+Class.signal.__get__
 
 instance = Class()
 instance.signal.emit
+instance.signal.connect
+instance.signal.disconnect
+instance.signal[str].emit
