57a2a1a139ebcff208980789ab42a99445a1ebd1
ignormies <bryce.beagle@gmail.com>
2020-10-07 21:53:48 -0700

    (WIP) Update the parameter type annotations for pyqtSignal.__init__
    The *types annotation is currently broken. Feedback appreciated

--- tests/pyqtsignal.py
+++ tests/pyqtsignal.py
@@ -3,13 +3,18 @@ from PyQt5 import QtCore
 
 
 class Class(QtCore.QObject):
-    signal = QtCore.pyqtSignal([str])  # type: typing.ClassVar[QtCore.pyqtSignal]
+    signal1 = QtCore.pyqtSignal([str])  # type: typing.ClassVar[QtCore.pyqtSignal]
+    signal2 = QtCore.pyqtSignal(int)
+    signal3 = QtCore.pyqtSignal([str, bool], [float])
+    signal4 = QtCore.pyqtSignal(int, name="cool_signal")
+    signal5 = QtCore.pyqtSignal(type, revision=2)
+    signal6 = QtCore.pyqtSignal(bool, arguments=["bool_arg"])
 
 
-Class.signal.__get__
+Class.signal1.__get__
 
 instance = Class()
-instance.signal.emit
-instance.signal.connect
-instance.signal.disconnect
-instance.signal[str].emit
+instance.signal1.emit
+instance.signal1.connect
+instance.signal1.disconnect
+instance.signal1[str].emit
