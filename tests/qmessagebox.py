0947ae651156536ceb237ff136077342717c5c04
Kyle Altendorf <sda@fstab.net>
2020-10-14 12:20:46 -0400

    Tests for QtWidgets.QMessageBox.StandardButtons

--- tests/qmessagebox.py
+++ tests/qmessagebox.py
@@ -2,3 +2,6 @@ from PyQt5 import QtWidgets
 
 a = QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Ok  # type: QtWidgets.QMessageBox.StandardButtons
 b = QtWidgets.QMessageBox.Ok | 0  # type: int
+c = a | 0  # type: QtWidgets.QMessageBox.StandardButtons
+d = a | QtWidgets.QMessageBox.Ok  # type: QtWidgets.QMessageBox.StandardButtons
+e = a | a  # type: QtWidgets.QMessageBox.StandardButtons
