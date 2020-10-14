b5a3124588d2942984fc608ac414eec936d15923
Kyle Altendorf <sda@fstab.net>
2020-10-14 00:19:42 -0400

    do not start creating gui objects...  yet anyways

--- tests/qmessagebox.py
+++ tests/qmessagebox.py
@@ -1,11 +1,4 @@
 from PyQt5 import QtWidgets
 
-QtWidgets.QMessageBox.question(
-    None,
-    "Confirm",
-    "Are you sure?",
-    QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
-)
-
 a: QtWidgets.QMessageBox.StandardButtons = QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Ok
 b: int = QtWidgets.QMessageBox.Ok | 0
