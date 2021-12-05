9b47c5653f11736e3b9bba12d4c0e8848ddc511f
Kyle Altendorf <sda@fstab.net>
2021-12-05 10:23:22 -0500

    macVersion is a function

--- PyQt5-stubs/QtCore.pyi
+++ PyQt5-stubs/QtCore.pyi
@@ -12417,7 +12417,8 @@ class QSysInfo(sip.simplewrapper):
             MV_WATCHOS_3_0 = ...  # type: int
             MV_YOSEMITE = ...  # type: int
 
-        macVersion = ...  # type: QSysInfo.MacintoshVersion
+        @staticmethod
+        def macVersion() -> 'QSysInfo.MacintoshVersion': ...
 
     @typing.overload
     def __init__(self) -> None: ...
