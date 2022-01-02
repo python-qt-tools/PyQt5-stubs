79e884f9d1194f8ee0d6c6e762cf61fd8ba300f0
Philippe F <phil.fremy@free.fr>
2022-01-02 22:00:09 +0100

    Python test example is working

--- tests/pyqtproperty.py
+++ tests/pyqtproperty.py
@@ -2,58 +2,83 @@
 # from PyQt5 documentation
 from PyQt5.QtCore import QObject, pyqtProperty, pyqtSignal
 
-class Total(QObject):
+
+# Python style properties
+class UseProps1(QObject):
 
     def __init__(self) -> None:
         QObject.__init__(self)
-        self._total = 0
+        self._prop1 = 0
+        self._prop2 = 0
 
-    # @pyqtProperty(int)
+    ### prop1
     @property
-    def total(self) -> int:
-        print('total getter')
-        return self._total
+    def prop1(self) -> int:
+        print('_prop1 getter')
+        return self._prop1
 
-    @total.setter
-    def total(self, value: int) -> None:
-        print('total setter')
-        self._total = value+100 # add + 100 to detect that we are running through the setter and because it's fun
+    @prop1.setter
+    def prop1(self, value: int) -> None:
+        print('_prop1 setter')
+        self._prop1 = value+100 # add + 100 to detect that we are running through the setter and because it's fun
+
+    @prop1.deleter
+    def prop1(self) -> None:
+        print('_prop1 deleter')
+        del self._prop1
 
 
-class Total2(QObject):
+    ### prop2
+    def set_prop2(self, value: int) -> None:
+        print('_prop2 setter')
+        self._prop2 = value+100 # add + 100 to detect that we are running through the setter and because it's fun
+
+    def get_prop2(self) -> int:
+        print('_prop2 getter')
+        return self._prop2
+
+    def del_prop2(self) -> None:
+        print('_prop2 deleter')
+        del self._prop2
+
+    prop2 = property(get_prop2, set_prop2, del_prop2)
+
+
+### Qt style properties
+class UseProps2(QObject):
 
     def __init__(self) -> None:
         QObject.__init__(self)
-        self._total2 = 0
+        self._prop3 = 0
 
-    def total2_getter(self) -> int:
-        print('total2 getter')
-        return self._total2
+    def prop3_getter(self) -> int:
+        print('prop3 getter')
+        return self._prop3
 
-    def total2_setter(self, v: int) -> None:
-        print('total2 setter')
-        self._total2 = v + 100  # add + 100 for fun
-        self.total2_sig_changed.emit()
+    def prop3_setter(self, v: int) -> None:
+        print('prop3 setter')
+        self._prop3 = v + 100  # add + 100 for fun
+        self.prop3_sig_changed.emit()
 
-    def total2_resetter(self) -> None:
-        print('total2 resetter')
-        self._total2 = 0
+    def prop3_resetter(self) -> None:
+        print('prop3 resetter')
+        self._prop3 = 0
 
-    def total2_deleter(self) -> None:
-        print('total2 deleter')
-        del self._total2
+    def prop3_deleter(self) -> None:
+        print('prop3 deleter')
+        del self._prop3
 
-    def total2_slot_changed(self) -> None:
-        print('total2 changed')
+    def prop3_slot_changed(self) -> None:
+        print('prop3 changed to ', self._prop3)
 
-    total2_sig_changed = pyqtSignal()
+    prop3_sig_changed = pyqtSignal()
 
-    total2 = pyqtProperty(
+    prop3 = pyqtProperty(
                     int,
-                    fget=total2_getter,
-                    fset=total2_setter,
-                    freset=total2_resetter,
-                    fdel=total2_deleter,
+                    fget=prop3_getter,
+                    fset=prop3_setter,
+                    freset=prop3_resetter,
+                    fdel=prop3_deleter,
                     doc='return the total using a Qt property',
                     designable=True,
                     scriptable=True,
@@ -61,66 +86,85 @@ class Total2(QObject):
                     user=True,
                     constant=True,
                     final=True,
-                    notify=total2_sig_changed,
+                    notify=prop3_sig_changed,
                     revision=1
     )
 
-class Total3(QObject):
+
+
+class UseProps3(QObject):
 
     def __init__(self) -> None:
         QObject.__init__(self)
-        self._total3 = 0
+        self._prop4 = 0
 
-    def total3_slot_changed(self, value: int) -> None:
-        print('total3 changed to', value)
+    def prop4_slot_changed(self, value: int) -> None:
+        print('prop4 changed to', value)
 
-    total3_sig_changed_1arg = pyqtSignal([int])
-    total3 = pyqtProperty(int, notify=total3_sig_changed_1arg)
+    prop4_sig_changed_1arg = pyqtSignal([int])
+    prop4 = pyqtProperty(int, notify=prop4_sig_changed_1arg)
 
     @pyqtProperty(int)
-    def total3(self) -> int:
-        print('total3 getter')
-        return self._total3
+    def prop4(self) -> int:
+        print('prop4 getter')
+        return self._prop4
 
-    @total3.getter
-    def total3(self) -> int:
-        print('total3 getter')
-        return self._total3
+    @prop4.setter
+    def prop4(self, v: int) -> None:
+        print('prop4 setter')
+        self._prop4 = v + 100      # add +100 for fun
+        self.prop4_sig_changed_1arg.emit(self._prop4)
 
-    @total3.setter
-    def total3(self, v: int) -> None:
-        print('total3 setter')
-        self._total3 = v + 100      # add +100 for fun
-
-    @total3.deleter
-    def total3(self) -> None:
-        print('total3 deleter')
-        del self._total3
+    @prop4.deleter
+    def prop4(self) -> None:
+        print('prop4 deleter')
+        del self._prop4
 
 
-print('==> Total')
-t = Total()
-print(t.total)
-t.total = 1
-print(t.total)
+print('==> Python style properties with UseProps1')
+up = UseProps1()
+print( up.prop1 )
+up.prop1 = 2
+print( up.prop1 )
+del up.prop1
 
-print('\n==> Total2')
-t2 = Total2()
-t2.total2_sig_changed.connect(t2.total2_slot_changed)
-t2.total2 = 2
-print(t2.total2)
-t2.total2_resetter()
-print(t2.total2)
-print(t2.total2.__doc__)
+print( up.prop2 )
+up.prop1 = 3
+print( up.prop2 )
+del up.prop2
+
+
+print('==> PyQt style properties with UseProps2')
+up2 = UseProps2()
+print(up2.prop3)
+up2.prop3_sig_changed.connect(up2.prop3_slot_changed)
+print('Setting to 2')
+up2.prop3 = 2
+print(up2.prop3)
+up2.setProperty('prop3', 3)
+print( up2.property('prop3') )
+
+print('Resetting')
+up2.prop3_resetter()
+print(up2.prop3)
+
+print(up2.prop3.__doc__)
 # final deletion
-del t2.total2
+del up2.prop3
 
-print('\n==> Total3')
-t3 = Total3()
-print(t3.total3)
-t3.total3_sig_changed_1arg.connect(t3.total3_slot_changed)
-t3.total3 = 3      # not working...
-del t3.total3
+print('\n==> PyQt/python style properties with UseProps3')
+up3 = UseProps3()
+print(up3.prop4)
+up3.prop4_sig_changed_1arg.connect(up3.prop4_slot_changed)
+print('Setting to 2')
+up3.prop4 = 2
+print(up3.prop4)
+up3.setProperty('prop4', 3)
+print( up3.property('prop4') )
+
+# final deletion
+del up3.prop4
+
 
 
 
