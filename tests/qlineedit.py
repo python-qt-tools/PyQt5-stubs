2bbcc22e7ccf5c8818340387971c05c291db1588
Philippe F <phil.fremy@free.fr>
2022-01-03 16:06:16 +0100

    Remove unneeded debug stuff

--- tests/qlineedit.py
+++ tests/qlineedit.py
@@ -1,9 +1,6 @@
 import os
 
-os.environ['QT_DEBUG_PLUGINS'] = '1'
-
 from PyQt5.QtWidgets import QLineEdit, QApplication
-from PyQt5.QtGui import QGuiApplication
 
 app = QApplication(['my_program', '-platform', 'offscreen'])
 
