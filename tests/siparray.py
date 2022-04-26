e296cfcab85f90993831bd01bc156abd9b6fb887
Philippe F <phil.fremy@free.fr>
2022-04-26 11:18:14 +0200

    More tests on voidptr (from #104)

--- tests/siparray.py
+++ tests/siparray.py
@@ -7,3 +7,9 @@ a = v.asarray(1)
 volume = QCustom3DVolume()
 volume.setTextureData(a)
 a = volume.textureData()
+
+# more voidptr tests
+v = sip.voidptr(None)
+v = sip.voidptr(b'123', 0)
+v = sip.voidptr(bytearray(b'123'), 0, False)
+v = sip.voidptr(v)
